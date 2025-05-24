031e54b2d2dff0dc298e7cdc5e3243e6c6f5c58b
kimgayatri <gayatrinehe6@gmail.com>
2025-05-24 08:27:42 +0530

    modified "GlobalApi.js"

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_KEY=import.meta.env.VITE_STRAPI_API_KEY;
 const axiosClient=axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL+"/api/",
+    baseURL:import.meta.env.VITE_BASE_URL+"/api/",
     //baseURL:'http://localhost:1337/api',
     //http://localhost:1337/
     headers:{
